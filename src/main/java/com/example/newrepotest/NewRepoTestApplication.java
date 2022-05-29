cc5565066c899d2964e4f5c80c0df36078697884
Thony <thon0101@stud.kea.dk>
2022-05-29 13:40:16 +0200

    First Commit

---
+++ src/main/java/com/example/newrepotest/NewRepoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.newrepotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewRepoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewRepoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newrepotest/NewRepoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newrepotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewRepoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
